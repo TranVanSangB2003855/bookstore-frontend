dbaeb459f3ee70069e36019774a7f6cb26d90758
TranVanSangB2003855 <96764199+TranVanSangB2003855@users.noreply.github.com>
2023-01-04 20:59:28 +0700

    Add files via upload

---
+++ assets/order.service.ff438252.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index.fb78404b.js";class t{constructor(r="/api/order"){this.api=e("https://bookstore-backend-production-eec1.up.railway.app/api/order")}async addOrder(r){return(await this.api.post("/",r)).data}async confirmOrder(r,a){return(await this.api.put(`/confirmorder/${r}`,a)).data}}const p=new t;export{p as O};
