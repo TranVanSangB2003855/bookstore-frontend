458c1d726771d3f72ea29b22ed7aeed39730795e
TranVanSangB2003855 <96764199+TranVanSangB2003855@users.noreply.github.com>
2023-01-04 10:25:43 +0700

    Delete assets directory

--- assets/order.service.4111eb38.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index.94399bba.js";class e{constructor(r="/api/order"){this.api=t(r)}async addOrder(r){return(await this.api.post("/",r)).data}async confirmOrder(r,a){return(await this.api.put(`/confirmorder/${r}`,a)).data}}const c=new e;export{c as O};
