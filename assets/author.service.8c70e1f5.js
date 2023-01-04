dc78b88d19fdb1df59dee4ce0f772bc781554cbe
TranVanSangB2003855 <96764199+TranVanSangB2003855@users.noreply.github.com>
2023-01-04 22:04:52 +0700

    Add files via upload

---
+++ assets/author.service.8c70e1f5.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index.2b10c86f.js";class r{constructor(t="/api/authors"){this.api=a("https://bookstore-backend-production-eec1.up.railway.app/api/authors")}async getAllAuthors(){return(await this.api.get("/")).data}async getAnAuthor(t){return(await this.api.get(`/${t}`)).data}async addAnAuthor(t){return(await this.api.post("/",t)).data}}const s=new r;export{s as A};
