15a56433efce8d13e4a2582ba3b0b815f21377a2
TranVanSangB2003855 <96764199+TranVanSangB2003855@users.noreply.github.com>
2023-01-04 21:55:35 +0700

    Add files via upload

---
+++ assets/order.service.f74e4f53.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index.c5306291.js";class e{constructor(r="/api/order"){this.api=t(r)}async addOrder(r){return(await this.api.post("/",r)).data}async confirmOrder(r,a){return(await this.api.put(`/confirmorder/${r}`,a)).data}}const c=new e;export{c as O};
