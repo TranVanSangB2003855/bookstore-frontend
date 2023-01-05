a1db6e990f32acca70d1abc3ef99b0efbad596cb
TranVanSangB2003855 <96764199+TranVanSangB2003855@users.noreply.github.com>
2023-01-05 11:44:20 +0700

    Delete assets directory

--- assets/author.service.b965e15b.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index.a236a14b.js";class r{constructor(t="/api/authors"){this.api=a("https://bookstore-backend-production-eec1.up.railway.app/api/authors")}async getAllAuthors(){return(await this.api.get("/")).data}async getAnAuthor(t){return(await this.api.get(`/${t}`)).data}async addAnAuthor(t){return(await this.api.post("/",t)).data}}const s=new r;export{s as A};
